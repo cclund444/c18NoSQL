3d469bca1fd477a32f43c6de143d8e0cd4aab9a2
cclund444 <87826114+cclund444@users.noreply.github.com>
2022-01-02 21:26:02 -0700

    update/change

--- Models/User.js
+++ Models/User.js
@@ -35,10 +35,6 @@ const UserSchema = new Schema({
     }
 );
 
-// UserSchema.virtual('friendCount').get(function() {
-//     return this.friends.length;
-// });
-
 
 const User = model('User', UserSchema);
 
