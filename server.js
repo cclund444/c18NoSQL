1ba2a4d9afd32298522451e8e04c5de9339e4e47
cclund444 <87826114+cclund444@users.noreply.github.com>
2022-01-12 08:10:36 -0700

    update code links

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/c18NoSQL', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
